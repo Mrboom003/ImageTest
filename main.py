5551f6aa40a93a18f373307520f14815ef5e2efa
Mustafa Patlar <o.mustafapatlar@gmail.com>
2025-12-23 21:58:40 +0300

    new Endpoint and svc added

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def test():
     return jsonify({"message": "Hello, World!"})
 
+
+@app.route('/new-endpoint', methods=['GET'])
+def new_endpoint():
+    return jsonify({"message": "This is a new endpoint!"})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
     
